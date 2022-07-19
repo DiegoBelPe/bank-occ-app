c755e4cf7ad3478538273fddb1c24e79a48292ef
DiegoBelPe <dbeltranpereira@gmail.com>
2022-07-19 07:31:13 -0500

    feat: responsive component CreditLines

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -4,7 +4,7 @@ import CircleAlert from './CircleAlert'
 const Alert = () => {
   return (
     <div className='container col-8 text-center d-flex flex-row justify-content-center  '>
-      <div class="col-8 alert alert-danger" role="alert">
+      <div class="col-12 col-md-8 alert alert-danger m-0" role="alert">
         <CircleAlert />
         Las demas lineas de credito no estarán disponibles por el momento!
       </div>
--- src/components/CreditLines.jsx
+++ src/components/CreditLines.jsx
@@ -12,16 +12,16 @@ const CreditLines = () => {
         <div className='row d-flex justify-content-center'>
         <div className="card col-4 col-md-4 col-lg-3 m-2 p-0 card_hover" >
           <img src={image.img2} className="card-img-top p-2 img_Credit" alt="..." />
-            <h5 className="card-title">Cartera Ordinaria</h5>
+            <h5 className="card_title">Cartera Ordinaria</h5>
             <div className="card-body  continue_btn">
-            <h4 className='fw-semibold'>Continuar</h4>
+            <h4 className='fw-semibold text_continue'>Continuar</h4>
           </div>
         </div>
         <div className="card col-4 col-md-4 col-lg-3 m-2 p-0 card_hover" >
           <img src={image.img3} className="card-img-top p-2 img_Credit" alt="..." />
-            <h5 className="card-title">Leasing</h5>
+            <h5 className="card_title">Leasing</h5>
           <div className="card-body continue_btn ">
-            <h4 className='fw-semibold'>Continuar</h4>
+            <h4 className='fw-semibold text_continue'>Continuar</h4>
           </div>
         </div>
         </div>
